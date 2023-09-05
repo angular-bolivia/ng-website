6f011a21b4bf308a58a119e61654b77716a66ee2
Mauricio Arce <mauricio.arce.mat@gmail.com>
2023-09-05 14:44:57 -0400

    Conver highlighted events logos into links

--- src/components/home/highlighted-events/highlighted-events.component.ts
+++ src/components/home/highlighted-events/highlighted-events.component.ts
@@ -12,11 +12,47 @@ import { Component } from '@angular/core';
           <p>
             {{ 'Algunos de nuestros eventos organizados:' | uppercase }}
           </p>
-          <div class="highlighted-events__logo">
-            <img src="/img/ng-bolivia-2018-logo.png" alt="Logo de NG-Bolivia 2018 de la comunidad Angular Bolivia" />
-            <img src="/img/ng-bolivia-2019-logo.png" alt="Logo de NG-Bolivia 2019 de la comunidad Angular Bolivia" />
-            <img src="/img/tsday-logo.png" alt="Logo de TSDay de la comunidad Angular Bolivia" />
-            <img src="/img/ng-she-logo.png" alt="Logo de NG-She de la comunidad Angular Bolivia" />
+          <div class="highlighted-events__logos">
+            <a
+              href="https://www.youtube.com/watch?v=Q3pp4bm5H3I&list=PLRejFvqDqGo6AMNYSLluAVGbq-CHQ0Lkt"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <img
+                src="/img/ng-bolivia-2018-logo.png"
+                alt="Logo de NG-Bolivia 2018 de la comunidad Angular Bolivia"
+              />
+            </a>
+            <a
+              href="https://www.youtube.com/watch?v=29w4cAjpMKA&list=PLRejFvqDqGo5moBGpb-A-QtZDjCMFFknf"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <img
+                src="/img/ng-bolivia-2019-logo.png"
+                alt="Logo de NG-Bolivia 2019 de la comunidad Angular Bolivia"
+              />
+            </a>
+            <a
+              href="https://www.youtube.com/live/_Gdygm9hBNA?si=LKauDOs0GqD4sKUl"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <img
+                src="/img/tsday-logo.png"
+                alt="Logo de TSDay de la comunidad Angular Bolivia"
+              />
+            </a>
+            <a
+              href="https://ngshe.ng-bolivia.org/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <img
+                src="/img/ng-she-logo.png"
+                alt="Logo de NG-She de la comunidad Angular Bolivia"
+              />
+            </a>
           </div>
         </div>
       </div>
