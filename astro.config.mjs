afe2736e74b7fd5216b49dcac203e8a1c1388f73
Mauricio Arce <mauricio.arce.mat@gmail.com>
2023-09-05 11:53:51 -0400

    Add Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-
 import analogjsangular from "@analogjs/astro-angular";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [analogjsangular()]
+  integrations: [analogjsangular(), partytown()]
 });
